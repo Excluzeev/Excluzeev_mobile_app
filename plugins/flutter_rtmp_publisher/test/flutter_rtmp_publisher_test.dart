0288d0edc8ad1cbf95f8fee8105aef21f2153671
Karthik <ponnamkarthik3@gmail.com>
2019-04-05 15:48:29 +0530

    removed sample test code from flutter rtmp player plugin

--- plugins/flutter_rtmp_publisher/test/flutter_rtmp_publisher_test.dart
+++ plugins/flutter_rtmp_publisher/test/flutter_rtmp_publisher_test.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_rtmp_publisher/flutter_rtmp_publisher.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_rtmp_publisher');
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
 }
